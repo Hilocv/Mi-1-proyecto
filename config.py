00c017ab43388089e8b74d1fab946d2e48a5f7ba
Hilocv <101907782+Hilocv@users.noreply.github.com>
2023-03-20 15:33:07 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+API_ID = '14681595'
+API_HASH = 'a86730aab5c59953c424abb4396d32d5'
+BOT_TOKEN = '6262289523:AAFxNkInr4lpsYTbKxwnjegz5UwY8ch4bCY'
+
+STARTED = '<b>Hola me llamo <a href="https://t.me/unzzip_bot">UNZIP_BOT</a> te ayudaré a gestionar tus archivos</b>'
+HELP = 'Hola soy un bot creado por la corporación <a href="https://t.me/nautaii">MC_BOTS</a> para hacer una herramienta útil'
